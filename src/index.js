7724dc5e43092c79d495e2421b15a78a0b207ed7
Gugulethu14 <gugulethu.madondo14@gmail.com>
2025-05-01 07:47:30 +0200

    added more API key intergration

--- src/index.js
+++ src/index.js
@@ -18,7 +18,6 @@ function refreshWeather(response){
   temperatureElement.innerHTML = Math.round(temperature);
 
 getForecast(response.data.city);
-
 }
 
 function formatDate(date){
@@ -47,6 +46,14 @@ function handleSearchSubmit(event){
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp){
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun","Mon","Tue","Wed","Thurs","Fri","Sat"];
+
+  return days[date.getDay()];
+
+}
+
 function getForecast(city){
   let apiKey = "ac1ob0c23605383a53a4b5df9ca98f4t";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -55,26 +62,25 @@ function getForecast(city){
 
 
 function displayForecast(response){
-  console.log(response.data);
-
-  let days = ["Tue","Wed","Thurs","Fri","Sat"];
   let forecastHtml = "";
 
-  days.forEach(function (day){
+  response.data.daily.forEach(function (day, index){
+    if (index < 6){
     forecastHtml =
     forecastHtml +
-
 `
 <div class="weather-forecast-day">
-  <div class="weather-forecast-date">${day}</div>
-  <div class="weather-forecast-icon">⛅</div>
+<div class="weather-forecast-date">${formatDay(day.time)}</div>
+
+  <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
   <div class="weather-forecast-temperatures">
- <div class="weather-forecast-temperature"><strong>15°</strong>
+ <div class="weather-forecast-temperature"><strong>${Math.round(day.temperature.maximum)}°</strong>
 </div>
-  <div class="weather-forecast-temperature">9°</div>
+  <div class="weather-forecast-temperature">${Math.round(day.temperature.minimum)}°</div>
 </div>
 </div>
 `;
+ }
 });
 
 let forecastElement = document.querySelector("#forecast");
@@ -86,4 +92,3 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit",handleSearchSubmit);
 
 searchCity("Soweto");
-displayForecast();
